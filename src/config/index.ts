211b147ce874b44bf1f3f16fba355f402f24bd0e
andrewpturner <andrew@2purple.net>
2025-01-15 19:58:52 +0000

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -45,75 +45,24 @@ export const common = {
   },
 }
 
-export const zh = {
-  ...common,
-  siteName: "小孙同学",
-  meta: {
-    ...common.meta,
-    title: "小孙同学",
-    slogan: "一个浪漫的理性主义者",
-    description: "读书、摄影、编程、旅行",
-  },
-  navigation: {
-    ...common.navigation,
-    custom: [
-      {
-        label: "影集",
-        link: "https://camlife.cn",
-      },
-    ],
-  },
-  pageMeta: {
-    archive: {
-      title: "归档",
-      description: "小孙同学的所有文章",
-      ogImage: "/images/page-meta/zh/archive.png",
-    },
-    links: {
-      title: "朋友们",
-      description: "小孙同学的和他朋友们",
-      ogImage: "/images/page-meta/zh/links.png",
-    },
-    about: {
-      title: "关于我",
-      description: "小孙同学的自我介绍",
-      ogImage: "/images/page-meta/zh/about.png",
-    },
-  },
-}
-
 export const en = {
   ...common,
-  siteName: "Guoqi Sun",
+  siteName: "Andrew P Turner",
   meta: {
     ...common.meta,
-    title: "Guoqi Sun",
-    slogan: "A Romantic Rationalist",
-    description: "Reading, Photography, Programming, Traveling",
-  },
-  navigation: {
-    ...common.navigation,
-    custom: [
-      {
-        label: "CamLife",
-        link: "https://camlife.cn",
-      },
-    ],
+    title: "Andrew P Turner",
+    slogan: "A digital garden where ideas flourish - from curious seedlings to lasting insights",
+    description: "Voice AI, Atomic Notes, Product Owner, Business Analysis, Bikepacking",
   },
   pageMeta: {
     archive: {
       title: "All Posts",
-      description: "Here are Guoqi Sun's all posts",
+      description: "Here are all the posts within this digital garden",
       ogImage: "/images/page-meta/en/archive.png",
     },
-    links: {
-      title: "My Friends",
-      description: "Here are Guoqi Sun's friends",
-      ogImage: "/images/page-meta/en/links.png",
-    },
     about: {
-      title: "About Me",
-      description: "Here is Guoqi Sun's self-introduction",
+      title: "About",
+      description: "About this website",
       ogImage: "/images/page-meta/en/about.png",
     },
   },

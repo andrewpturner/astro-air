6e1a76d045bdc20192be9ab9713e66038dc092d9
andrewpturner <andrew@2purple.net>
2025-01-15 20:07:32 +0000

    Update links.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -4,11 +4,5 @@ export const links = [
     link: "https://astro.build",
     description: "The web framework for content-driven websites",
     avatar: "/links/astro.svg",
-  },
-  {
-    name: "Guoqi Sun",
-    link: "https://blog.sunguoqi.com",
-    description: "Try, fail, retry. That's the rhythm of growth.",
-    avatar: "https://assets.guoqi.dev/images/avatar.png",
-  },
+  }
 ]
